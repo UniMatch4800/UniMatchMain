2b9cf17e204cd66ebc8e329efad4d6e5e317fccc
David Achemian <realdavidachemian@gmail.com>
2023-11-19 18:07:23 -0800

    Account Page, prior to multiple pages

--- src/screens/accountFiles/account.js
+++ src/screens/accountFiles/account.js
@@ -1,9 +1,22 @@
-import React from "react";
-import { auth, signOut } from "../../firebase"; // Ensure these are exported from your firebase.js
+import React, { useState, useEffect } from "react";
+import { auth, signOut, updateProfile, db, storage } from "../../firebase";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { useNavigate } from "react-router-dom";
 import "./account.css";
+
 function Account() {
   const navigate = useNavigate();
+  const [newName, setNewName] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [profileURL, setProfileURL] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  useEffect(() => {
+    if (auth.currentUser) {
+      setDisplayName(auth.currentUser.displayName);
+      setProfileURL(auth.currentUser.photoURL);
+    }
+  }, []);
 
   const handleLogOut = async () => {
     try {
@@ -14,13 +27,99 @@ function Account() {
     }
   };
 
+  const handleNameChange = async (e) => {
+    e.preventDefault();
+    try {
+      await updateProfile(auth.currentUser, { displayName: newName });
+      setDisplayName(newName); // Update the local state to reflect the change
+      alert("Name updated successfully");
+    } catch (error) {
+      console.error("Error updating name: ", error);
+    }
+  };
+
+  const handleEmailChange = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.currentUser.updateEmail(newEmail);
+      alert("Email updated successfully");
+    } catch (error) {
+      console.error("Error updating email: ", error);
+    }
+  };
+
+  const handleProfilePhotoChange = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    const storageRef = ref(storage, `profilePhotos/${auth.currentUser.uid}`);
+    try {
+      await uploadBytes(storageRef, file);
+      const photoURL = await getDownloadURL(storageRef);
+      await updateProfile(auth.currentUser, { photoURL });
+      setProfileURL(photoURL); // Update the local state to reflect the change
+      alert("Profile photo updated successfully");
+    } catch (error) {
+      console.error("Error updating profile photo: ", error);
+    }
+  };
+
   return (
-    <div>
-      <h1>Account Page</h1>
-      <p>Welcome to your account page.</p>
+    <div className="account-page">
+      <h1 className="account-heading">Account Page</h1>
+
+      {/* Profile Display Section */}
+      <div className="profile-section">
+        <img
+          src={profileURL || "default-profile-placeholder.png"}
+          alt="Profile"
+          className="profile-photo"
+        />
+        <h2 className="profile-name">{displayName || "Your Name"}</h2>
+        {/* Additional profile information can go here */}
+      </div>
+
       <button onClick={handleLogOut} className="account-button">
         Log Out
       </button>
+
+      <div className="form-section">
+        <form onSubmit={handleNameChange}>
+          <label className="form-label">Change Name:</label>
+          <input
+            className="account-input"
+            type="text"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder="Enter new name"
+          />
+          <button type="submit" className="account-button">
+            Update Name
+          </button>
+        </form>
+
+        <form onSubmit={handleEmailChange}>
+          <label className="form-label">Change Email:</label>
+          <input
+            className="account-input"
+            type="email"
+            value={newEmail}
+            onChange={(e) => setNewEmail(e.target.value)}
+            placeholder="Enter new email"
+          />
+          <button type="submit" className="account-button">
+            Update Email
+          </button>
+        </form>
+
+        <div>
+          <label className="form-label">Upload Profile Photo:</label>
+          <input type="file" onChange={handleProfilePhotoChange} />
+          <button onClick={handleProfilePhotoChange} className="account-button">
+            Update Photo
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
