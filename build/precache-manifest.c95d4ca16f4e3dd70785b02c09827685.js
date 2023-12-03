b91b1e0f18d7aab811ce45aeb64ca4e3f974b44b
FIRST_NAME LAST_NAME <davitbarseg@gmail.com>
2023-12-03 13:34:46 -0800

    build files

---
+++ build/precache-manifest.c95d4ca16f4e3dd70785b02c09827685.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "723e10656d3dd16ee3ef",
+    "url": "/static/css/main.d75ecba5.chunk.css"
+  },
+  {
+    "revision": "723e10656d3dd16ee3ef",
+    "url": "/static/js/main.723e1065.chunk.js"
+  },
+  {
+    "revision": "032afe787ef41b32deee",
+    "url": "/static/css/1.3c7d2295.chunk.css"
+  },
+  {
+    "revision": "032afe787ef41b32deee",
+    "url": "/static/js/1.032afe78.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4f19d54fc5cacaba85bad4ef6ce8180a",
+    "url": "/static/media/loadingGif.4f19d54f.gif"
+  },
+  {
+    "revision": "d9508e612eeb53810298e5f0e76cfe20",
+    "url": "/static/media/ULyfe_final_logo.d9508e61.PNG"
+  },
+  {
+    "revision": "d1fd08b6099f1e0e60dd932dcadcb3a4",
+    "url": "/index.html"
+  }
+];
